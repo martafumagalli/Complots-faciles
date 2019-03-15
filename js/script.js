9dec732618e98cc2fce4631dedf2d065a57a9403
Marta Fumagalli <fumagalli.marta@gmail.com>
2019-03-15 11:07:11 +0100

    cloned master

---
+++ js/script.js
@@ -0,0 +1,78 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+
+
+
+
+// MODAL
+
+function memory() {
+  // var visited = sessionStorage['visited'];
+  // if (!visited) {
+     myModal();
+  //    sessionStorage['visited'] = true;
+  // }
+
+  function myModal(){
+    // const modalOuvert = document.getElementById('open-modal');
+    const modalModal = document.getElementById('modal');
+    const modalBody = document.getElementById('body');
+    const modalFerme = document.getElementById('close-modal');
+    const modalExtend = document.getElementById('extend-modal');
+    
+
+    
+    modalModal.style.display = "block";
+    modalBody.style.opacity = "0.7";
+      
+    
+    modalFerme.onclick = function() {
+      document.getElementById("modal").innerHTML = '<div id ="modal-inner" class="modal-inner content center"><h2>Le christ cosmique te souhaite la bienvenue!</h2><img class="petit" src="https://image.jimcdn.com/app/cms/image/transf/dimension=833x10000:format=jpg/path/sf4bd355bb25ee14b/image/i8b1817e0cf8b2b4c/version/1490549180/image.jpg" style=" width:100px height:100px"> <br><br><button id="modal-inner">Bienvenue mon ami !</button></div>';
+      const modalInner = document.getElementById('modal-inner');
+  
+  
+      modalInner.onclick = function() {
+        modalModal.style.display = "none";
+        modalBody.style.opacity = "1";
+        
+        }
+
+
+    }
+
+    modalExtend.onclick = function() {
+    document.getElementById("modal").innerHTML = '<div class="modal-inner content center"><h2>Que le christ cosmique te pardonne</h2><p>NOUS NE VOULONS PAS DE REPTILIENS HUMANOIDES</p><br><br><button id="modal-inner">Dégage !</button></div>';
+    const modalInner = document.getElementById('modal-inner');
+    
+
+    modalInner.onclick = function() {
+      modalModal.style.display = "none";
+      modalBody.style.opacity = "1";
+      window.open('https://media.giphy.com/media/l2Je3fAJ02BkvLYEE/giphy.gif');
+      }
+    }
+  }
+}
+
+
+window.onload = setTimeout(memory,1000);
+
+
+
+
+
